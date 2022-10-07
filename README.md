f06de79c7504075207e51c78d094355c3311f6de
A682156 <dhruv.patel@adm.com>
2022-10-07 10:46:14 -0500

    Re-desing Application for Rocket Central

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Rocket Task
+# Rocket Central Task
 
 ## In orde to run this project install node module with help of command:
 
 npm install
 
-## Development server
+## Development Server to Run
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthorService } from './author.service'
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { AuthorService } from './author.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
- authorList:any = []
 
-  constructor(private authorService: AuthorService){
+  @ViewChild('concernedDiv') concernedDiv: ElementRef;
+  authorList: any = [];
+  hiddenItems: any = {};
+
+  changeOpacity: boolean = false;
+  active:boolean = false;
+
+  constructor(private authorService: AuthorService) {}
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 100) {
+      this.changeOpacity = true;
+    } else {
+      this.changeOpacity = false;
+    }
 
   }
 
-
   ngOnInit() {
-    this.authorService.getAuthorsList().subscribe((data:any) => this.authorList = data)
+    this.authorService
+      .getAuthorsList()
+      .subscribe(
+        (data: any) =>
+          (this.authorList = data.map((item) => ({ ...item, show: false })))
+      );
+  }
+
+  trackById(index: number, item:{id: string}) {
+    return item.id;
+  }
+
+  toggleExpansion(index) {
+    this.active = !this.active;
+  }
+
+  show() {
+    this.concernedDiv.nativeElement.classList.add('_Menus-show')
+  }
+
+  hide() {
+    this.concernedDiv.nativeElement.classList.remove('_Menus-show')
   }
 }
